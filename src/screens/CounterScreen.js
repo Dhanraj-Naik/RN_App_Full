f1ef20457042b36482b6263731e53dc1641c8ee5
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2021-02-04 19:29:32 +0530

    -modified counter screen to use useReducer -square screen useReducer made more generaized

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,29 +1,42 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
+
+const reducer = (state, action) => {
+    //state === { counter : number}
+    //action === { type === 'increase_value' || 'decrease_value', payload : 15 || -15}
+    switch (action.type) {
+        case 'increase_value':
+            return { ...state, counter: state.counter + action.payload };
+        case 'decrease_value':
+            return { ...state, counter: state.counter + action.payload };
+
+        default:
+            state;
+    }
+};
 
 const CounterScreen = ({
     params,
 }) => {
-    let [counter, setCounter] = useState(0);
+
+    const [state, dispatch] = useReducer(reducer, { counter : 0});
 
     return (
         <View style={styles.containerstyle}>
             <Button
                 title={'INCREASE'}
                 onPress={() => {
-                    counter++;
-                    setCounter(counter);
+                    dispatch({ type: 'increase_value', payload: 1 });
                 }}
             />
             <Button
                 title={'DECREASE'}
                 onPress={() => {
-                    counter--;
-                    setCounter(counter);
+                    dispatch({ type: 'decrease_value', payload: -1 });
                 }}
             />
             <Text style={styles.textStyle}>Current Counter: </Text>
-            <Text style={styles.textCounterStyle}>{counter}</Text>
+            <Text style={styles.textCounterStyle}>{state.counter}</Text>
         </View>
     );
 };
